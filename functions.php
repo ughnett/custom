37a000a06421ebc6f23e1c6cecfb78b83a231be9
Annette Ruchala <ughnett@me.com>
2018-10-15 04:27:02 -0500

    Update functions.php

---
+++ functions.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Theme customizations
+ *
+ * @package	Custom
+ * @author	Annette Ruchala
+ * @link	https://github.com/ughnett/Custom
+ * @copyright	Copyright (c) 2018, Annette Ruchala
+ * @license	GPL-2.0+
+ */
