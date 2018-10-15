5e31f9fd2aa2fb79115236a5ba522f2956182263
Annette Ruchala <ughnett@me.com>
2018-10-15 04:49:10 -0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -5,14 +5,47 @@
  *
  * @package	Custom
  * @author	Annette Ruchala
- * @link	https://github.com/ughnett/Custom
+ * @link	https://github.com/ughnett/custom
  * @copyright	Copyright (c) 2018, Annette Ruchala
  * @license	GPL-2.0+
  */
 
-add_action( 'Genesis_setup', 'custom_setup',15);
-
-  // Load child theme textdomain.
+// Load child theme textdomain.
 load_child_theme_textdomain( 'custom' );
+add_action( 'genesis_setup', 'custom_setup',15);
+function custom_setup() {
+	// Define theme constants.
+	define( 'CHILD_THEME_NAME', 'Custom' );
+	define( 'CHILD_THEME_URL', 'https://github.com/ughnett/custom' );
+	define( 'CHILD_THEME_VERSION', '1.0.0' );
 
-function custom_setup(){}
+		// Add HTML5 markup structure.
+	add_theme_support( 'html5', array( 'comment-list', 'comment-form', 'search-form', 'gallery', 'caption'  ) );
+	
+	// Add viewport meta tag for mobile browsers.
+	add_theme_support( 'genesis-responsive-viewport' );
+	
+	// Add theme support for accessibility.
+	add_theme_support( 'genesis-accessibility', array(
+		'404-page',
+		'drop-down-menu',
+		'headings',
+		'rems',
+		'search-form',
+		'skip-links',
+	) );
+	// Add theme support for footer widgets.
+	add_theme_support( 'genesis-footer-widgets', 3 );
+		// Unregister layouts that use secondary sidebar.
+	genesis_unregister_layout( 'content-sidebar-sidebar' );
+	genesis_unregister_layout( 'sidebar-content-sidebar' );
+	genesis_unregister_layout( 'sidebar-sidebar-content' );
+	// Unregister secondary sidebar.
+	unregister_sidebar( 'sidebar-alt' );
+}
+// Add Google Font stylesheet.
+add_action( 'wp_enqueue_scripts', 'custom_equeue_styles' );
+function custom_equeue_styles() {
+	wp_enqueue_style( 'google-fonts', '//fonts.googleapis.com/css?family=Roboto:400,400italic,700,700italic|Lobster' );
+	
+}
